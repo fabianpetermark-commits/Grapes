17a4291677e83cebf4c3719d1bbe26ee1cb7d175
fabianpetermark-commits <fabian.peter.mark@gmail.com>
2026-09-18 22:34:25 +0200

    Wire QR Studio navigation

--- src/main.js
+++ src/main.js
@@ -13,8 +13,9 @@ import { showScreen, showModulePicker } from './screens.js'
 
 el('#pick-brochure').addEventListener('click', () => showScreen('brochure'))
 el('#pick-studio').addEventListener('click', () => showScreen('studio'))
+el('#pick-qr').addEventListener('click', () => showScreen('qr'))
 
-for (const selector of ['#app-back-to-menu-btn', '#studio-back-to-menu-btn', '#fabric-back-to-menu-btn']) {
+for (const selector of ['#app-back-to-menu-btn', '#studio-back-to-menu-btn', '#fabric-back-to-menu-btn', '#qr-back-to-menu-btn']) {
   el(selector).addEventListener('click', showModulePicker)
 }
 
